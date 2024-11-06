94f61ee7b5a5f10330c135450d9aeda40b97deb6
soham parab <parabsoham01@gmail.com>
2024-11-06 18:44:10 +0530

    try: export fixes

--- plugnplay-hooks/src/App.jsx
+++ plugnplay-hooks/src/App.jsx
@@ -1,6 +1,9 @@
 import "./App.css";
+import { useIsOnline } from "hooks-on-the-go";
 
 function App() {
+  const abcd = useIsOnline();
+  console.log(abcd);
   return (
     <>
       <div>Plug n play: an easy way to use React hooks</div>
