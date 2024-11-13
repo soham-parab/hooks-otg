eaf55123bc6f0b315c156f09ed4a00c72262cb46
soham parab <parabsoham01@gmail.com>
2024-11-13 20:19:15 +0530

    fix:try

--- plugnplay-hooks/vite.config.js
+++ plugnplay-hooks/vite.config.js
@@ -1,13 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import { fileURLToPath } from "url";
 
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     lib: {
-      entry: path.resolve("src/index.js"),
+      entry: path.resolve(__dirname, "src/index.js"),
       name: "hooks-on-the-go",
       formats: ["es", "cjs"],
       fileName: (format) => `index.${format === "es" ? "js" : "cjs"}`,
