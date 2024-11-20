ca24f8f87534e744bc13944fb4cec934e034b8bd
soham parab <parabsoham01@gmail.com>
2024-11-20 11:21:48 +0530

    hooksOTG: 0.1.2

--- plugnplay-hooks/src/App.jsx
+++ plugnplay-hooks/src/App.jsx
@@ -1,5 +1,7 @@
 import "./App.css";
+import { useIsOnline } from "hooks-on-the-go";
 function App() {
+  const isOnline = useIsOnline();
   return (
     <>
       <div>Plug n play: an easy way to use React hooks</div>
